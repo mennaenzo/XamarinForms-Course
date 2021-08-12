fdd6cf8e322546f1b243a09ff5f0f125bfd2f238
mennaenzo <enzo.menna94@gmail.com>
2021-08-12 17:55:03 -0300

    27. Borrando selecciones

--- CollectionViewDemo/CollectionViewDemo/ViewModels/PeoplePageViewModel.cs
+++ CollectionViewDemo/CollectionViewDemo/ViewModels/PeoplePageViewModel.cs
@@ -2,17 +2,26 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace CollectionViewDemo.ViewModels
 {
-    public class PeoplePageViewModel
+    public class PeoplePageViewModel : INotifyPropertyChanged
     {
         private Person selectedPerson;
         private List<object> selectedPeople;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public ObservableCollection<Person> People { get; set; }
 
@@ -22,6 +31,7 @@ namespace CollectionViewDemo.ViewModels
             set
             {
                 selectedPerson = value;
+                OnPropertyChanged();
             }
         }
 
@@ -31,11 +41,13 @@ namespace CollectionViewDemo.ViewModels
             set
             {
                 selectedPeople = value;
+                OnPropertyChanged();
             }
         }
 
         public ICommand PersonChangedCommand { get; set; }
         public ICommand PeopleChangedCommand { get; set; }
+        public ICommand ClearCommand { get; set; }
 
 
         public PeoplePageViewModel()
@@ -56,6 +68,11 @@ namespace CollectionViewDemo.ViewModels
             PeopleChangedCommand = new Command((p) =>
             {
                 var peopleList = p;
+            });
+
+            ClearCommand = new Command(() =>
+            {
+                SelectedPeople = null;
             });
         }
 
