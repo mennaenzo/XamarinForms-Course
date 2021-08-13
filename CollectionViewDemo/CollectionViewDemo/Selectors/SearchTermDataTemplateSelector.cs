9a6aaad8f393dcbfce943e07877fa8ac53a413d4
mennaenzo <enzo.menna94@gmail.com>
2021-08-13 15:40:51 -0300

    33. Seleccionando un EmptyViewTemplate en tiempo de ejecución

---
+++ CollectionViewDemo/CollectionViewDemo/Selectors/SearchTermDataTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using CollectionViewDemo.CustomTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CollectionViewDemo.Selectors
+{
+    public class SearchTermDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+        public DataTemplate SecondaryTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            string query = ((FilterData)item).Filter;
+
+            if(query.ToLower().Equals("xamarin"))
+            {
+                return DefaultTemplate;
+            }
+            else
+            {
+                return SecondaryTemplate;
+            }
+        }
+    }
+}
