16ad13b941e02a975db507122316fea768599bd4
mennaenzo <enzo.menna94@gmail.com>
2021-08-13 14:49:58 -0300

    32. Desplegando un tipo personalizado con plantilla

---
+++ CollectionViewDemo/CollectionViewDemo/CustomTypes/FilterData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CollectionViewDemo.CustomTypes
+{
+    public class FilterData : BindableObject
+    {
+        public static readonly BindableProperty FilterProperty = BindableProperty.Create(nameof(Filter), typeof(String), typeof(FilterData), null);
+        private string filter;
+
+        public String Filter
+        {
+            get { return (String) GetValue(FilterProperty); }
+            set
+            {
+                SetValue(FilterProperty, value);
+            }
+        }
+    }
+}
