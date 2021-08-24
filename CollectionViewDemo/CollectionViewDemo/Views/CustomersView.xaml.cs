70620c219a284f616266e5fce1dc0a947b4b4281
mennaenzo <enzo.menna94@gmail.com>
2021-08-24 12:02:07 -0300

    39. Detectando el Scrolling

--- CollectionViewDemo/CollectionViewDemo/Views/CustomersView.xaml.cs
+++ CollectionViewDemo/CollectionViewDemo/Views/CustomersView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using CollectionViewDemo.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,17 @@ namespace CollectionViewDemo.Views
         {
             InitializeComponent();
             BindingContext = new CustomersViewModel();
+        }
+
+        private void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            Debug.WriteLine("HorizontalDelta: " + e.HorizontalDelta);
+            Debug.WriteLine("VerticalDelta: " + e.VerticalDelta);
+            Debug.WriteLine("HorizontalOffset: " + e.HorizontalOffset);
+            Debug.WriteLine("VerticalOffset: " + e.VerticalOffset);
+            Debug.WriteLine("FirstVisibleItemIndex: " + e.FirstVisibleItemIndex);
+            Debug.WriteLine("CenterItemIndex: " + e.CenterItemIndex);
+            Debug.WriteLine("LastVisibleItemIndex: " + e.LastVisibleItemIndex);
         }
     }
 }
